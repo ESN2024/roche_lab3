0ec457f46aa62b4631f558f01c444f179a445f53
clementrch <clementrch01@gmail.com>
2024-01-16 11:36:13 +0100

    burstmode function 1/2

--- soft/app/main.c
+++ soft/app/main.c
@@ -3,20 +3,39 @@
 #include "altera_avalon_pio_regs.h"
 #include "opencores_i2c.h"
 
-int main() {
+unsigned int I2C_READ_ADXL345() {
 
-    __uint32_t X0 ; 
-    I2C_init(OPENCORES_I2C_0_BASE,ALT_CPU_CPU_FREQ,400000); // 
-
+    unsigned int data;
 
     I2C_start(OPENCORES_I2C_0_BASE, 0x1D, 0);
     I2C_write(OPENCORES_I2C_0_BASE,0x32,0);
     I2C_start(OPENCORES_I2C_0_BASE, 0x1D, 1);
 
-    X0 = I2C_read(OPENCORES_I2C_0_BASE, 0);
+    data = I2C_read(OPENCORES_I2C_0_BASE, 0);
+}
 
-    alt_printf("%x\n", X0);
+int main() {
 
+    __uint32_t X0,X1,Y0 ; 
+    I2C_init(OPENCORES_I2C_0_BASE,ALT_CPU_CPU_FREQ,400000); // 
+
+    while(1) {
+        I2C_start(OPENCORES_I2C_0_BASE, 0x1D, 0);
+        I2C_write(OPENCORES_I2C_0_BASE,0x32,0);
+        I2C_start(OPENCORES_I2C_0_BASE, 0x1D, 1);
+        X0 = I2C_read(OPENCORES_I2C_0_BASE, 0);
+
+        I2C_write(OPENCORES_I2C_0_BASE,0x33,0);
+        I2C_start(OPENCORES_I2C_0_BASE, 0x1D, 1);
+        X1 = I2C_read(OPENCORES_I2C_0_BASE, 0);
+
+        I2C_write(OPENCORES_I2C_0_BASE,0x34,0);
+        I2C_start(OPENCORES_I2C_0_BASE, 0x1D, 1);
+        Y0 = I2C_read(OPENCORES_I2C_0_BASE, 1);
+
+        alt_printf("X1 : %x, X0 : %x \n", X1, X0);
+        alt_printf("Y0 : %x", Y0);
+    }
     return 0;
 }
 
