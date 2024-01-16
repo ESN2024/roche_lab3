625342ff93cf052d2e092af333912e8ffdaa642d
clementrch <clementrch01@gmail.com>
2024-01-16 13:33:04 +0100

    lecture de tous les registres data du gyroscope ok

--- soft/app/main.c
+++ soft/app/main.c
@@ -3,20 +3,25 @@
 #include "altera_avalon_pio_regs.h"
 #include "opencores_i2c.h"
 
-unsigned int I2C_READ_ADXL345() {
+// unsigned int I2C_READ_ADXL345() {
 
-    unsigned int data;
+//     __uint32_t X0,X1,Y0,Y1,Z0,Z1; 
 
-    I2C_start(OPENCORES_I2C_0_BASE, 0x1D, 0);
-    I2C_write(OPENCORES_I2C_0_BASE,0x32,0);
-    I2C_start(OPENCORES_I2C_0_BASE, 0x1D, 1);
+//     I2C_start(OPENCORES_I2C_0_BASE, 0x1D, 0);
+//     I2C_write(OPENCORES_I2C_0_BASE,0x32,0);
+//     I2C_start(OPENCORES_I2C_0_BASE, 0x1D, 1);
+//     X0 = I2C_read(OPENCORES_I2C_0_BASE, 0);
 
-    data = I2C_read(OPENCORES_I2C_0_BASE, 0);
-}
+//     I2C_write(OPENCORES_I2C_0_BASE,0x32,0);
+//     I2C_start(OPENCORES_I2C_0_BASE, 0x1D, 1);
+//     X0 = I2C_read(OPENCORES_I2C_0_BASE, 0);
+
+//     data = I2C_read(OPENCORES_I2C_0_BASE, 0);
+// }
 
 int main() {
 
-    __uint32_t X0,X1,Y0 ; 
+    __uint16_t X0,X1,Y0,Y1,Z0,Z1 ; 
     I2C_init(OPENCORES_I2C_0_BASE,ALT_CPU_CPU_FREQ,400000); // 
 
     while(1) {
@@ -25,18 +30,34 @@ int main() {
         I2C_start(OPENCORES_I2C_0_BASE, 0x1D, 1);
         X0 = I2C_read(OPENCORES_I2C_0_BASE, 0);
 
+        I2C_start(OPENCORES_I2C_0_BASE, 0x1D, 0);
         I2C_write(OPENCORES_I2C_0_BASE,0x33,0);
         I2C_start(OPENCORES_I2C_0_BASE, 0x1D, 1);
         X1 = I2C_read(OPENCORES_I2C_0_BASE, 0);
 
+        I2C_start(OPENCORES_I2C_0_BASE, 0x1D, 0);
         I2C_write(OPENCORES_I2C_0_BASE,0x34,0);
         I2C_start(OPENCORES_I2C_0_BASE, 0x1D, 1);
-        Y0 = I2C_read(OPENCORES_I2C_0_BASE, 1);
+        Y0 = I2C_read(OPENCORES_I2C_0_BASE, 0);
 
-        alt_printf("X1 : %x, X0 : %x \n", X1, X0);
-        alt_printf("Y0 : %x", Y0);
+        I2C_start(OPENCORES_I2C_0_BASE, 0x1D, 0);
+        I2C_write(OPENCORES_I2C_0_BASE,0x35,0);
+        I2C_start(OPENCORES_I2C_0_BASE, 0x1D, 1);
+        Y1 = I2C_read(OPENCORES_I2C_0_BASE, 0);
+
+        I2C_start(OPENCORES_I2C_0_BASE, 0x1D, 0);
+        I2C_write(OPENCORES_I2C_0_BASE,0x36,0);
+        I2C_start(OPENCORES_I2C_0_BASE, 0x1D, 1);
+        Z0 = I2C_read(OPENCORES_I2C_0_BASE, 0);
+
+        I2C_start(OPENCORES_I2C_0_BASE, 0x1D, 0);
+        I2C_write(OPENCORES_I2C_0_BASE,0x37,0);
+        I2C_start(OPENCORES_I2C_0_BASE, 0x1D, 1);
+        Z1 = I2C_read(OPENCORES_I2C_0_BASE, 1);
+
+        alt_printf("X : %x%x Y : %x%x Z : %x%x\n", X1,X0,Y1,Y0,Z1,Z0);
+
+        usleep(1000000);
     }
     return 0;
 }
-
-
