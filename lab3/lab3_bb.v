7e7ba872eba4fa58effd729cda29327477f2df51
clementrch <clementrch01@gmail.com>
2024-01-16 08:41:45 +0100

    design qsys avec ip block i2c

---
+++ lab3/lab3_bb.v
@@ -0,0 +1,14 @@
+
+module lab3 (
+	clk_clk,
+	opencores_i2c_0_export_0_scl_pad_io,
+	opencores_i2c_0_export_0_sda_pad_io,
+	pio_0_external_connection_export,
+	reset_reset_n);	
+
+	input		clk_clk;
+	inout		opencores_i2c_0_export_0_scl_pad_io;
+	inout		opencores_i2c_0_export_0_sda_pad_io;
+	output	[7:0]	pio_0_external_connection_export;
+	input		reset_reset_n;
+endmodule
---
+++ lab3/lab3_inst.v
@@ -0,0 +1,8 @@
+	lab3 u0 (
+		.clk_clk                             (<connected-to-clk_clk>),                             //                       clk.clk
+		.opencores_i2c_0_export_0_scl_pad_io (<connected-to-opencores_i2c_0_export_0_scl_pad_io>), //  opencores_i2c_0_export_0.scl_pad_io
+		.opencores_i2c_0_export_0_sda_pad_io (<connected-to-opencores_i2c_0_export_0_sda_pad_io>), //                          .sda_pad_io
+		.pio_0_external_connection_export    (<connected-to-pio_0_external_connection_export>),    // pio_0_external_connection.export
+		.reset_reset_n                       (<connected-to-reset_reset_n>)                        //                     reset.reset_n
+	);
+
