50b118ac0cac2d5945ef29a8e908befe7e66dc6d
clementrch <clementrch01@gmail.com>
2024-01-18 18:55:02 +0100

    calibration OK
    Calibration sur le plan Z validé et ajout de fonction pour avoir en sortie de l'accelerometre un resultat en mg

--- soft/app/main.c
+++ soft/app/main.c
@@ -4,7 +4,7 @@
 #include "opencores_i2c.h"
 #include <stdint.h>
 
-unsigned int I2C_READ_ADXL345(__uint8_t reg) {
+__uint8_t I2C_READ_ADXL345(__uint8_t reg) {
 
     __uint8_t data_out;    
 
@@ -41,18 +41,24 @@ void I2C_READ_BURSTMODE_ADXL345(__uint8_t *X0, __uint8_t *X1, __uint8_t *Y0, __u
     *Z1 = I2C_READ_ADXL345(0x37);
 }
 
+__int16_t complement_2(__int16_t number){
+    if (number & 0x8000) {
+        number = -(0xFFFF) - number + 1;
+    }
+    return number;
+}
 
 
 int main() {
 
     __uint8_t X0,X1,Y0,Y1,Z0,Z1 ; 
-    __uint16_t X, Y, Z;
+    __int16_t X, Y, Z;
     __uint8_t OFSX, OFSY, OFSZ; 
     I2C_init(OPENCORES_I2C_0_BASE,ALT_CPU_CPU_FREQ,400000); // 
 
     I2C_WRITE_ADXL345(0x1E, 0x0);
     I2C_WRITE_ADXL345(0x1F, 0x05);
-    I2C_WRITE_ADXL345(0x20, 0xE5);
+    I2C_WRITE_ADXL345(0x20, 0xCD);
 
     I2C_WRITE_ADXL345(0x31, 0x7); //full range
 
@@ -66,19 +72,21 @@ int main() {
 
     while(1) {
         I2C_READ_BURSTMODE_ADXL345(&X0,&X1,&Y0,&Y1,&Z0,&Z1);
-        X = X1 << 8;
-        X = X + X0;
 
-        Y = Y1 << 8;
-        Y = Y + Y0;
+        X = (X1 << 8) + X0;
 
-        Z = Z1 << 8;
-        Z = Z + Z0;
+        Y = (Y1 << 8) + Y0;
+
+        Z = (Z1 << 8) + Z0;
+
+        X = complement_2(X)*3.9; //permet d avoir un resultat en mg
+        Y = complement_2(Y)*3.9;
+        Z = complement_2(Z)*3.9;
 
         // alt_printf("X : %x.%x Y : %x.%x Z : %x.%x\n", X1,X0,Y1,Y0,Z1,Z0);
         // alt_printf("X : %x\n", OFSZ);
         alt_printf("X : %x Y : %x Z : %x\n", X, Y, Z);
-        usleep(10000);
+        usleep(900000);
     }
     return 0;
 }
