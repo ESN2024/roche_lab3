843165ce1b02b9a89a54412aedac07c0df28d4cf
clementrch <clementrch01@gmail.com>
2024-01-19 21:02:30 +0100

    ajout d'un timer
    actualisation toute les secondes des valeurs sur les 7segments via un timer avec une interruption

--- soft/app/main.c
+++ soft/app/main.c
@@ -10,6 +10,9 @@
 #include "opencores_i2c.h"
 #include <stdint.h>
 
+volatile __int16_t X, Y, Z;
+volatile __uint8_t X0,X1,Y0,Y1,Z0,Z1,m,u,d,c,negatif;
+
 __uint8_t I2C_READ_ADXL345(__uint8_t reg) {
 
     __uint8_t data_out;    
@@ -81,46 +84,46 @@ void display_segment(__int16_t X, __uint8_t *negatif) {
     IOWR_ALTERA_AVALON_PIO_DATA(PIO_3_BASE, m);
 }
 
+static void irqhandler_timer(void* context, alt_u32 id)
+{
+    
+
+    I2C_READ_BURSTMODE_ADXL345(&X0,&X1,&Y0,&Y1,&Z0,&Z1);
+
+    X = (X1 << 8) + X0;
+
+    Y = (Y1 << 8) + Y0;
+
+    Z = (Z1 << 8) + Z0;
+
+    X = complement_2(X,&negatif)*3.9; //permet d avoir un resultat en mg
+    display_segment(X, &negatif);
+
+    IOWR_ALTERA_AVALON_TIMER_STATUS(TIMER_0_BASE, 0x1); //clear le flag d'interruption
+}
+
+
+
 int main() {
 
-    __uint8_t X0,X1,Y0,Y1,Z0,Z1,m,u,d,c,negatif;
-    __int16_t X, Y, Z;
-    __uint8_t OFSX, OFSY, OFSZ; 
+    
+    
+    // __uint8_t OFSX, OFSY, OFSZ; 
     I2C_init(OPENCORES_I2C_0_BASE,ALT_CPU_CPU_FREQ,400000); // 
 
-    I2C_WRITE_ADXL345(0x1E, 0x0);
-    I2C_WRITE_ADXL345(0x1F, 0x05);
-    I2C_WRITE_ADXL345(0x20, 0xCD);
+    // I2C_WRITE_ADXL345(0x1E, 0x0);
+    // I2C_WRITE_ADXL345(0x1F, 0x05);
+    // I2C_WRITE_ADXL345(0x20, 0xCD);
 
     I2C_WRITE_ADXL345(0x31, 0x7); //full range
 
-    OFSX = I2C_READ_ADXL345(0x1E);
-    OFSY = I2C_READ_ADXL345(0x1F);
-    OFSZ = I2C_READ_ADXL345(0x20);
+    // OFSX = I2C_READ_ADXL345(0x1E);
+    // OFSY = I2C_READ_ADXL345(0x1F);
+    // OFSZ = I2C_READ_ADXL345(0x20);
 
     // alt_printf("OFS : %x   %x   %x\n", OFSX, OFSY, OFSZ);
 
-    usleep(500000);
-
-    while(1) {
-        I2C_READ_BURSTMODE_ADXL345(&X0,&X1,&Y0,&Y1,&Z0,&Z1);
-
-        X = (X1 << 8) + X0;
-
-        Y = (Y1 << 8) + Y0;
-
-        Z = (Z1 << 8) + Z0;
-
-        X = complement_2(X,&negatif)*3.9; //permet d avoir un resultat en mg
-        // Y = complement_2(Y,&negatifY)*3.9;
-        // Z = complement_2(Z,&negatifZ)*3.9;
-
-        display_segment(X, &negatif);
-
-        // alt_printf("X : %x.%x Y : %x.%x Z : %x.%x\n", X1,X0,Y1,Y0,Z1,Z0);
-        // alt_printf("X : %x\n", OFSZ);
-        alt_printf("X : %x Y : %x Z : %x\n", X, Y, Z);
-        usleep(900000);
-    }
+    alt_irq_register(TIMER_0_IRQ, NULL, irqhandler_timer);
+    while(1) {}
     return 0;
 }
