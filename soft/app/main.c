a083469df5a9c0ecd81aff98c0ecad33f2ddc77d
clementrch <clementrch01@gmail.com>
2024-01-21 11:48:21 +0100

    ajout de commentaire

--- soft/app/main.c
+++ soft/app/main.c
@@ -13,7 +13,7 @@
 volatile __int16_t X, Y, Z;
 volatile __uint8_t X0,X1,Y0,Y1,Z0,Z1,m,u,d,c,negatif,bouton;
 
-__uint8_t I2C_READ_ADXL345(__uint8_t reg) {
+__uint8_t I2C_READ_ADXL345(__uint8_t reg) { // Lis un registre
 
     __uint8_t data_out;    
 
@@ -26,7 +26,7 @@ __uint8_t I2C_READ_ADXL345(__uint8_t reg) {
     return data_out;
 }
 
-void I2C_WRITE_ADXL345(__uint8_t reg, __uint8_t data_in) {  
+void I2C_WRITE_ADXL345(__uint8_t reg, __uint8_t data_in) {  // Ecrit dans un regstre "data_in"
 
     I2C_start(OPENCORES_I2C_0_BASE, 0x1D, 0);
 
@@ -34,13 +34,13 @@ void I2C_WRITE_ADXL345(__uint8_t reg, __uint8_t data_in) {
     I2C_write(OPENCORES_I2C_0_BASE,data_in,1);
 }
 
-void ADXL345_SET_OFFSET_0() { 
+void ADXL345_SET_OFFSET_0() { //Set l'offset à 0
     I2C_WRITE_ADXL345(0x1E, 0x0);
     I2C_WRITE_ADXL345(0x1F, 0x0);
     I2C_WRITE_ADXL345(0x20, 0x0);
 }
 
-void I2C_READ_BURSTMODE_ADXL345(__uint8_t *X0, __uint8_t *X1, __uint8_t *Y0, __uint8_t *Y1, __uint8_t *Z0, __uint8_t *Z1) {
+void I2C_READ_BURSTMODE_ADXL345(__uint8_t *X0, __uint8_t *X1, __uint8_t *Y0, __uint8_t *Y1, __uint8_t *Z0, __uint8_t *Z1) { // Lis tous les registre des valeurs de sortie
 
     *X0 = I2C_READ_ADXL345(0x32);
     *X1 = I2C_READ_ADXL345(0x33);
@@ -50,7 +50,7 @@ void I2C_READ_BURSTMODE_ADXL345(__uint8_t *X0, __uint8_t *X1, __uint8_t *Y0, __u
     *Z1 = I2C_READ_ADXL345(0x37);
 }
 
-__int16_t complement_2(__int16_t number, __uint8_t *negatif){
+__int16_t complement_2(__int16_t number, __uint8_t *negatif){ //Calcul le complement a 2 si le nombre est negatif et mets a 1 le pointeur negatif
     if (number & 0x8000) {
         number = -(0xFFFF) - number + 1;
         *negatif = 1;
@@ -60,79 +60,52 @@ __int16_t complement_2(__int16_t number, __uint8_t *negatif){
     return number;
 }
 
-void display_segment(__int16_t X, __uint8_t *negatif) {
+void display_segment(__int16_t X, __uint8_t *negatif) { // affiche sur 5 digits le nombre (1 digit pour le signe et 4 pour le nombre)
 
     __uint8_t m,u,d,c;
 
+    // Calcul BCD d'un nombre de 4 chiffres
     m = X / 1000;           
     c = (X / 100) % 10;     
     d = (X / 10) % 10;      
     u = X % 10;             
 
     if (*negatif) {
-        IOWR_ALTERA_AVALON_PIO_DATA(PIO_4_BASE, 0b1111); //mets le signe -
+        IOWR_ALTERA_AVALON_PIO_DATA(PIO_4_BASE, 0b1111); //mets le signe - car dans le .vhdl on initialise - avec 0b1111
     } else {
-        IOWR_ALTERA_AVALON_PIO_DATA(PIO_4_BASE, 0b1011); //affiche rien
+        IOWR_ALTERA_AVALON_PIO_DATA(PIO_4_BASE, 0b1011); //affiche rien (signe +)
     }
 
-    IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE, u); 
+    IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE, u); // affiche l'unite sur le digit0
 
-    IOWR_ALTERA_AVALON_PIO_DATA(PIO_1_BASE, d);
+    IOWR_ALTERA_AVALON_PIO_DATA(PIO_1_BASE, d); // affiche les dizaine sur le digit1
 
-    IOWR_ALTERA_AVALON_PIO_DATA(PIO_2_BASE, c);
+    IOWR_ALTERA_AVALON_PIO_DATA(PIO_2_BASE, c); // affiche les centaines sur le digit2
 
-    IOWR_ALTERA_AVALON_PIO_DATA(PIO_3_BASE, m);
+    IOWR_ALTERA_AVALON_PIO_DATA(PIO_3_BASE, m); // affiche les millieme sur le digit3
 }
 
-static void irqhandler_timer(void* context, alt_u32 id)
+static void irqhandler_timer(void* context, alt_u32 id) 
 {
     
-
+    // Lecture des donnees
     I2C_READ_BURSTMODE_ADXL345(&X0,&X1,&Y0,&Y1,&Z0,&Z1);
 
+    // Concatenation des donnees sur 16 bits
     X = (X1 << 8) + X0;
-
     Y = (Y1 << 8) + Y0;
-
     Z = (Z1 << 8) + Z0;
-    alt_printf("btn : %x", bouton);
-    // switch(bouton){
-    //     case 0:
-    //         X = complement_2(X,&negatif)*3.9; //permet d avoir un resultat en mg
-    //         display_segment(X, &negatif);
-    //         break;
-    //     case 1:
-    //         X = complement_2(X,&negatif)*3.9; //permet d avoir un resultat en mg
-    //         display_segment(X, &negatif);
-    //         break;
-    //     case 2:
-    //         Y = complement_2(Y,&negatif)*3.9; //permet d avoir un resultat en mg
-    //         display_segment(Y, &negatif);
-    //         break;
-    //     case 3:
-    //         Y = complement_2(Y,&negatif)*3.9; //permet d avoir un resultat en mg
-    //         display_segment(Y, &negatif);
-    //         break;
-    //     case 4:
-    //         Z = complement_2(Z,&negatif)*3.9; //permet d avoir un resultat en mg
-    //         display_segment(Z, &negatif);
-    //         break;
-    //     case 5:
-    //         Z = complement_2(Z,&negatif)*3.9; //permet d avoir un resultat en mg
-    //         display_segment(Z, &negatif);
-    //         break;
-    // }
 
+
+    // Affichage des donnees suivant la valeur de bouton (bouton = (0=X;1=Y;2=Z))
     if (bouton==0){
         X = complement_2(X,&negatif)*3.9; //permet d avoir un resultat en mg
         display_segment(X, &negatif);
     } 
-
     if (bouton==1){
         Y = complement_2(Y,&negatif)*3.9; //permet d avoir un resultat en mg
         display_segment(Y, &negatif);
     } 
-
     if (bouton==2){
         Z = complement_2(Z,&negatif)*3.9; //permet d avoir un resultat en mg
         display_segment(Z, &negatif);
@@ -146,35 +119,42 @@ static void irqhandler_bouton_key1(void* context, alt_u32 id)
     // Réinitialiser le registre de capture de bord pour effacer l'interruption
     IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PIO_BOUTON_BASE, 0x1);
 
-    if(bouton>2){
+    // Incremente ou RAZ de bouton
+    if(bouton>1){
         bouton=0;
     } else {
         bouton++;
     }
-    
+    alt_printf("bouton (0=X;1=Y;2=Z) : %x \n", bouton);
     // Action de réponse à l'interruption
 
 }
 
 int main() {
     bouton=0;
-    // __uint8_t OFSX, OFSY, OFSZ; 
+    __uint8_t OFSX, OFSY, OFSZ; 
     I2C_init(OPENCORES_I2C_0_BASE,ALT_CPU_CPU_FREQ,400000); // 
 
-    // I2C_WRITE_ADXL345(0x1E, 0x0);
+    // Plan Z
+    I2C_WRITE_ADXL345(0x1E, 0x0);
+    I2C_WRITE_ADXL345(0x1F, 0x05);
+    I2C_WRITE_ADXL345(0x20, 0xCD);
+
+    // I2C_WRITE_ADXL345(0x1E, 2048);
     // I2C_WRITE_ADXL345(0x1F, 0x05);
-    // I2C_WRITE_ADXL345(0x20, 0xCD);
+    // I2C_WRITE_ADXL345(0x20, 1794);
 
     I2C_WRITE_ADXL345(0x31, 0x7); //full range
 
-    // OFSX = I2C_READ_ADXL345(0x1E);
-    // OFSY = I2C_READ_ADXL345(0x1F);
-    // OFSZ = I2C_READ_ADXL345(0x20);
+    OFSX = I2C_READ_ADXL345(0x1E);
+    OFSY = I2C_READ_ADXL345(0x1F);
+    OFSZ = I2C_READ_ADXL345(0x20);
 
     // alt_printf("OFS : %x   %x   %x\n", OFSX, OFSY, OFSZ);
 
+
+    // Enregistrer les interruptions
     alt_irq_register(TIMER_0_IRQ, NULL, irqhandler_timer);
-    // Enregistrer l'interruption
     IOWR_ALTERA_AVALON_PIO_IRQ_MASK(PIO_BOUTON_BASE, 0x1);
     IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PIO_BOUTON_BASE, 0x1);
     alt_irq_register(PIO_BOUTON_IRQ, NULL, irqhandler_bouton_key1);
